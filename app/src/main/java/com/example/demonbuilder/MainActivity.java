3df11f372598a487c091e13c1c6db5b2d2577999
PRINCESANCHEZ <67511950+PRINCESANCHEZ@users.noreply.github.com>
2023-12-04 20:25:11 -0800

    Started SignUp page! Added some color! Changed app logo!

--- app/src/main/java/com/example/demonbuilder/MainActivity.java
+++ app/src/main/java/com/example/demonbuilder/MainActivity.java
@@ -1,8 +1,17 @@
+/**
+ * @author Prince Sanchez
+ * @since December 4, 2023
+ * Description: Bodybuilding app!
+ */
 package com.example.demonbuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get references to your buttons
+        Button signUpButton = findViewById(R.id.button);
+        Button logInButton = findViewById(R.id.button2);
+
+        // Set click listener for the Sign Up button
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpIntent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+        // Set click listener for the Log In button
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
